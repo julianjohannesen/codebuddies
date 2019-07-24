8bc68438233dc4437b2f6e6ee52e4656865bc0cf
julianjohannesen <github email address>
2019-07-24 11:19:55 -0400

    More manual merging and updating a lot of packages

--- client/templates/admin/report/report_hangout_modal.js
+++ client/templates/admin/report/report_hangout_modal.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-Template.reportHangoutModal.helpers({
-  create: function(){
-
-  },
-  rendered: function(){
-
-  },
-  destroyed: function(){
-
-  },
-});
-
-Template.reportHangoutModal.events({
-  'click #report-hangout': function(e) {
-
-    var report = {
-      hangoutId: Session.get("hangoutId"),
-      category: $('input[name="report-category"]:checked').val(),
-      reporterId: Meteor.userId()
-    };
-
-    Meteor.call('reportHangout', report, function(err, result) {
-      if (result) {
-        Modal.hide();
-        sweetAlert({
-          title: TAPi18n.__("hangout_has_been_reported"),
-          text: TAPi18n.__("hangout_reported_message"),
-          confirmButtonText: TAPi18n.__("ok"),
-          type: 'success',
-          closeOnConfirm: true
-        });
-      }
-    });
-  }
-
-});
-=======
 Template.reportHangoutModal.helpers({
   create: function() {},
   rendered: function() {},
@@ -63,4 +25,3 @@ Template.reportHangoutModal.events({
     });
   }
 });
->>>>>>> upstream/staging
