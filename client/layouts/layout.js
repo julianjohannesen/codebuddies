97d308ebc59faa2330291d9a9ecca23c60089c2c
Julian Johannesen <julian.johannesen@gmail.com>
2019-07-23 20:47:47 -0400

    manually accepting incoming changes can't be right

--- client/layouts/layout.js
+++ client/layouts/layout.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-Template.layout.events({
-  "click .continue-popup": function(event, template){
-    event.preventDefault();
-    if (!Meteor.userId()) {
-      sweetAlert({
-        imageUrl: '/images/slack-signin-example.jpg',
-        imageSize: '140x120',
-        title: TAPi18n.__("you_are_almost_there"),
-        html: TAPi18n.__("continue_popup_text"),
-        showCancelButton: true,
-        confirmButtonText: TAPi18n.__("continue_with_slack"),
-        cancelButtonText: TAPi18n.__("not_now"),
-
-      },
-      function(){
-        var options = {
-          requestPermissions: ['identify', 'users:read']
-        };
-        Meteor.loginWithSlack(options);
-      });
-    }
-
-  }
-});
-=======
 Template.layout.events({
   "click .continue-popup": function(event, template) {
     event.preventDefault();
@@ -63,4 +37,3 @@ Template.layout.events({
   },
   "click .signInGithub": function(event) {}
 });
->>>>>>> upstream/staging
